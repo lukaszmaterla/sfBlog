44910312bfea2becd298b494aea889f2779e203a
lukasz <lukasz@student.edu>
2017-05-07 19:35:51 +0200

    comments to articles added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Comment;
+use AppBundle\Entity\Post;
+use AppBundle\Form\CommentType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -25,5 +29,35 @@ class DefaultController extends Controller
         return $this->render('::base.html.twig', array(
             'posts'=> $pagination
         ));
+    }
+
+    /**
+     * @Route("/article/{id}", name="post_show")
+     * @Template(":Article:show.html.twig")
+     */
+    public function showAction(Post $post, Request $request)
+    {
+
+        $comment = new Comment();
+        $comment->setPost($post);
+
+        //$comment->setUser($user);
+        $form = $this->createForm(new CommentType(), $comment);
+        $form->handleRequest($request);
+        if ($form->isValid()&& $form->isSubmitted()){
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('success', "Komentarz został dodany");
+
+            return $this->redirectToRoute('post_show', ['id'=>$post->getId()]);
+
+        }
+        return [
+            'post'=>$post,
+            'form'=> $form->createView()
+        ];
     }
 }
