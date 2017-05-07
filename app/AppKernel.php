cf60e1029a87cb2eec5e79679628ff16d8f30300
lukasz <lukasz@student.edu>
2017-05-07 20:36:59 +0200

    admin panel added, application detect admin  user, and log this user as admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
         );
